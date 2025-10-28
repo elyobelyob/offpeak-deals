7155b814a79eff8eb4ad3a33aa2d7bbb107d4bc2
elyobelyob <nick@elyob.com>
2025-10-28 00:40:41 +0000

    Create config.phpAdd php/config.php

---
+++ php/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
+define('DB_USER', getenv('DB_USER') ?: 'appuser');
+define('DB_PASS', getenv('DB_PASS') ?: 'secretpass');
+define('DB_NAME', getenv('DB_NAME') ?: 'offpeak');
+?>
