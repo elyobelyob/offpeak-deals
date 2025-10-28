2ba97c47be59716599650ad6fbae95999d7f9233
elyobelyob <nick@elyob.com>
2025-10-28 00:55:18 +0000

    Create dashboard.phpAdd business dashboard skeleton

---
+++ php/business/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../config.php';
+session_start();
+// TODO: Add authentication and role check for business owners
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Business Dashboard</title>
+</head>
+<body>
+    <h1>Business Dashboard</h1>
+    <p>Welcome to the business portal. Here you can manage your business locations and deals.</p>
+</body>
+</html>
